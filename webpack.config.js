4512c72a316ecd84108aecfbcd268988bfb57553
DimaMaslo88 <dmitrij-maslo@mail.ru>
2024-04-16 22:17:24 +0300

    'feat/html'

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 
 const path = require('path')
-
+const HTMLPlugin = require('html-webpack-plugin')
 module.exports={
      entry: './src/app.js',
      output: {
-         filename: 'bundle.js',
+         filename: 'bundle.[chunkhash].js',
          path: path.resolve(__dirname,'public')
      },
     devServer: {
          port:3000
-    }
+    },
+    plugins: [
+        new HTMLPlugin({
+            template: "./src/index.html"
+        })
+    ]
 }
 
