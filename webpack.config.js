df1c62dd764b4873ff8b4f1f03762d76c7fe9e5a
DimaMaslo88 <dmitrij-maslo@mail.ru>
2024-04-17 23:00:00 +0300

    'feat/html-css'

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports={
      entry: './src/app.js',
      output: {
@@ -13,7 +14,13 @@ module.exports={
     plugins: [
         new HTMLPlugin({
             template: "./src/index.html"
-        })
-    ]
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules:[
+            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
+        ]
+    },
 }
 
